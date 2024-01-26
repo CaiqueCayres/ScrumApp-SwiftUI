80cf5048532fddd74468c4b8fd235827cd1a8fe2
Carlos Henrique <caiquecayres@gmail.com>
2024-01-25 23:22:39 -0300

    Handling Errors

--- ScrumApp/ScrumAppApp.swift
+++ ScrumApp/ScrumAppApp.swift
@@ -25,6 +25,7 @@ struct ScrumAppApp: App {
     }()
     
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
     
     var body: some Scene {
         
@@ -35,19 +36,38 @@ struct ScrumAppApp: App {
                 Task {
                     
                     do {
+                        
                         try await store.save(scrums: store.scrums)
+                        
                     } catch {
-                        fatalError(error.localizedDescription)
+                        
+                        errorWrapper = ErrorWrapper(error: error,
+                                                    guidance: "Try again later.")
+                        
                     }
                 }
             }
             .task {
                 
                 do {
+                    
                     try await store.load()
+                    
                 } catch {
-                    fatalError(error.localizedDescription)
+                    
+                    errorWrapper = ErrorWrapper(error: error,
+                                                guidance: "Scrumdinger will load sample data and continue.")
+                    
                 }
+            }
+            .sheet(item: $errorWrapper) {
+                
+                store.scrums = DailyScrum.sampleData
+                
+            } content: { wrapper in
+                
+                ErrorView(errorWrapper: wrapper)
+                
             }
         }
         .modelContainer(sharedModelContainer)
